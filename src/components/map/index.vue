63f1e4386ec0f1ac3de381c66984753bd840d23a
LJuvo <www.704921698@qq.com>
2022-08-22 16:09:52 +0800

    添加多级瓦片

--- src/components/map/index.vue
+++ src/components/map/index.vue
@@ -19,7 +19,7 @@ export default {
       this.map = L.map('map', {
         center: [40.02404009136253, 116.50641060224784], // 地图中心
         zoom: 14, // 缩放比列
-        zoomControl: false, // 禁用 + - 按钮
+        zoomControl: true, // 禁用 + - 按钮
         doubleClickZoom: false, // 禁用双击放大
         attributionControl: false // 移除右下角leaflet标识
       })
